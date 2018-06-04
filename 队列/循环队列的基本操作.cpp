e46620b9b3fb07836ade2b17b381caa63ae077b9
WANGT21 <13576149791@163.com>
2018-06-04 13:13:17 +0800

    Add files via upload

--- 队列/循环队列的基本操作.cpp
+++ 队列/循环队列的基本操作.cpp
@@ -1,96 +1,58 @@
-#include <stdio.h>
-#include <malloc.h>
+ #include <stdio.h>
+ #include <stdlib.h>
+ #define OK 1
+ #define ERROR 0
+ #define OVERFLOW -2
+ #define MAXQSIZE 100
+ typedef int Status;
+ typedef int QElemType;
+ typedef struct node{
+	 QElemType *base;
+	 int front;
+	 int rear;
+ }sqQueue;
+ //初始化创建一个循环队列
+ Status initqueue(sqQueue* q){
+	q->base=(QElemType*)malloc(sizeof(QElemType));
+	if(!q->base){
+		exit(OVERFLOW);
+	}
+	q->front=q->rear=0;
+	return OK;
+ }
+/*
+队列满条件：（rear+1)%MaxSize=front
+队列空条件：rear=front
+*/
+ //循环队列进元素
+ Status enqueue(sqQueue *q,QElemType elem){
+	 //队列为空时 1%100==1,队列满时(99+1)%100==0,最多容纳99个元素
+	if ((q->rear+1)%MAXQSIZE==q->front)
+	{
+		return ERROR;
+	}
+	q->base[q->rear]=elem;
+	q->rear=(q->rear+1)%MAXQSIZE;//rear始终在0-100中循环
+	return OK;
+ }
+ //循环队列出元素
 
-typedef enum{
-    OK=0;
-    ERROR=1;
-    TRUE=2;
-    FALSE=3;
+ Status dequeue(sqQueue *q,QElemType &elem){
+	if (q->rear==q->front)
+	{
+		return ERROR;
+	}
+	elem=q->base[q->front];
+	q->front=(q->front+1)%MAXQSIZE;
+	return OK;
+ }
 
-}status;
-typedef int elemtype;
-
-typedef struct node{
-    node *next;
-    elemtype data;
-}lqnode;
-
-typedef struct{
-    lqnode *front;
-    lqnode *rear;
-}linkqueue;
-//创建一个链队列
-status initqueue(linkqueue *phead){
-    phead->front=phead->rear=(lqnode*)malloc(sizeof(lqnode));
-    if (!phead->front)
-    {
-        printf("pQHead->front malloc error!\n");
-        return ERROR;
-    }
-    phead->front->next = NULL;
-    
-    return OK;
-	
-} 
-//销毁链队列
-void destoryqueue(linkqueue *phead){
-    free(phead->front);
-    free(phead->rear);
-    phead->front=phead->rear=NULL;
-} 
-//清空链队列
-void clearQueue(linkqueue *phead){
-    phead->front=phead->rear;   
-} 
-//判断是否为空
-status isEmpty(linkqueue *phead){
-    if (phead->front=phead->rear)
-    {
-        return TRUE;
-    }
-    return FALSE;
-}
-int getQueLen(linkqueue *phead){
-    lqnode *temp=phead->front;
-    int length=0;
-    while(temp!=phead->rear){
-        ++length;
-        temp=temp->next;
-    }
-    return length;
-}
-//进队列
-status enqueue(linkqueue *phead,elemtype element){
-    lqnode *temp=(lqnode*)malloc(sizeof(lqnode));
-    if(!temp)
-    {
-        printf("temp malloc error!\n");
-        return ERROR;
-    }
-    temp->data=element;
-    temp->next= NULL;
-    
-    phead->rear->next=temp;
-	
-    phead->rear=temp;
-	
-}
-//出队列
-status dequeue(linkqueue *phead,elemtype &element){
-    lqnode *temp=phead->front->next;
-    if (isEmpty(phead)==TRUE)
-    {
-        printf("queue is NULL!\n");
-        return ERROR;
-    }
-    if (phead->front->next=phead->rear)
-    {
-        phead->rear=phead->front;//防止尾指针指向空，所以提前将它指向头指针
-    }
-    element=temp->data;
-    phead->front->next=temp->next;
-    free(temp);
-    temp=NULL;
-	
-    return OK;
-}
+//遍历循环队列
+ Status printqueue(sqQueue q){
+	printf("the queue is:");
+	for (int i=q.front;i<q.rear;++i)
+	{
+		printf("%d ",q.base[i]);
+	}
+	return OK;
+ }
